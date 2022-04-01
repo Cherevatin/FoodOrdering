e485a22e7fb49c3489d6507ce637bc443157e5d6
Сергей Череватин <cherevatin3356@gmail.com>
2022-04-01 18:56:16 +0300

    Add Init Migration

---
+++ FoodOrdering.Infrastructure/Migrations/20220401155358_Initial-creation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FoodOrdering.Infrastructure.Migrations
+{
+    public partial class Initialcreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DishesMenus",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ReadyToOrder = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DishesMenus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dishes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Ingredients = table.Column<string>(type: "text", nullable: true),
+                    Price = table.Column<double>(type: "double precision", nullable: false),
+                    ImageName = table.Column<string>(type: "text", nullable: true),
+                    Proteins = table.Column<double>(type: "double precision", nullable: true),
+                    Fats = table.Column<double>(type: "double precision", nullable: true),
+                    Carbohydrates = table.Column<double>(type: "double precision", nullable: true),
+                    Calories = table.Column<double>(type: "double precision", nullable: true),
+                    Weight = table.Column<double>(type: "double precision", nullable: false),
+                    DishesMenuId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dishes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dishes_DishesMenus_DishesMenuId",
+                        column: x => x.DishesMenuId,
+                        principalTable: "DishesMenus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dishes_DishesMenuId",
+                table: "Dishes",
+                column: "DishesMenuId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dishes");
+
+            migrationBuilder.DropTable(
+                name: "DishesMenus");
+        }
+    }
+}
